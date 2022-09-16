7037ae676b6d4edb5f621c5d9ca22902070fa78a
Joseph T <jdtoronto999@outlook.com>
2022-09-16 17:25:37 +0100

    Made list thingy

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+        label : "Week 01",
+        url : "week01/"
+    }
+];
+
+let linkList = document.querySelector("ol");
+
+links.forEach(link => {
+    let linkLabel = document.createElement("li");
+    let linkUrl = document.createElement("a");
+
+    linkUrl.textContent = link.label;
+    linkUrl.setAttribute("href", link.url);
+
+    linkLabel.appendChild(linkUrl);
+    linkList.appendChild(linkLabel);
+});
